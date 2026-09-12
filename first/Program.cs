1c4c83b59cff27f747b08beff68b2ec2e0dc9719
krqwe <krqwe@511-02>
2026-09-12 14:39:15 +0200

    Добавьте файлы проекта.

---
+++ first/Program.cs
@@ -0,0 +1,20 @@
+﻿////Console.WriteLine("Введите ваше имя");
+//////string name = console .Readline();
+//////Console.Writeline("Меня зовут" name);
+//////Console.Writeline("Меня зовут {$}" ,name);
+//////Console.Writeline($"меня зовут {name}");
+//////Console.Write("Введите x:");
+//////int x = int.Parse(Console.Readline());
+//////int s = x + y;
+//////Console.Writeline($"[x]+[y]=[s]");
+////float x = 3,7F;
+////double v -Math.Pow(x, 7)
+////Console.Write("Введите угол в градусах:");
+////double angle = double.Parse(Console.ReadLine());
+////double y = Math.Sin(angle) * Math.PI / 180);
+////Console.WriteLine($"y={y:f2}");
+//double number = double .Parse(Console.ReadLine()`);
+//double y = Math.Exp(7 * number);
+////Console.WriteLine($"y={y:f2}"); 
+
+//Лаборат
---
+++ lab1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Transactions;
+
+//Console.Write("Введите x:");
+//double x = double.Parse(Console.ReadLine());
+//double K = (Math.Sqrt(Math.Pow(3+x,6) - Math.Log
+//    (x))) / (Math.Exp(0) + Math.Asin(6*x*x));
+//Console.WriteLine($"K={K:F2}");
+
+//высокий 7
+Console.Write("Введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите y");
+double y = double.Parse(Console.ReadLine());
+Console.Write("Введите a");
+double a = double.Parse(Console.ReadLine());
+double D = (Math.Cos(Math.Pow(x, 3) + 6) - Math.Sin(y - a)) / (Math.Log(Math.Pow(x, 4)) - 2 * (Math.Pow(Math.Sin(x), 5)));
+Console.WriteLine($"D={D:F2}");
+
+
