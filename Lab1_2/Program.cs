d6d01375a762543d81ced9ddc55f3c0f20677ee6
krqwe <krqwe@511-02>
2026-09-18 18:12:59 +0200

    lab1_2

---
+++ Lab1_2/Program.cs
@@ -0,0 +1,29 @@
+﻿//try
+//{
+//    Console.Write("Введите первую переменную");
+//        double x = double.Parse(Console.ReadLine());
+//    Console.Write("Введите вторую переменную");
+//        double y = double.Parse(Console.ReadLine());
+//    double avg = (Math.Pow(x, 3) + Math.Pow(y, 3)) / 2
+//        Console.WriteLine($"Среднее арифмитическое):{avg:"F2"}");
+//}
+//    catch(Exception ex)
+//    {
+//    Console.WriteLine(Exception.Message);
+//}*/
+
+try
+{
+    Console.Write("Введите Массу (m):");
+    double m = double.Parse(Console.ReadLine());
+    double k = 100;
+    double T = 2 * Math.PI * Math.Sqrt(m / k);
+    Console.WriteLine($"Период колебаний");
+    { T: string v = "F2"; }
+    ;
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
--- lab1_1/Program.cs
+++ lab1_1/Program.cs
@@ -7,11 +7,11 @@
 //Console.WriteLine($"K={K:F2}");
 
 //высокий 7
-Console.Write("Введите x:");
+Console.Write("Введите y:");
 double x = double.Parse(Console.ReadLine());
-Console.Write("Введите y");
+Console.Write("Введите r");
 double y = double.Parse(Console.ReadLine());
-Console.Write("Введите a");
+Console.Write("Введите t");
 double a = double.Parse(Console.ReadLine());
 double D = (Math.Cos(Math.Pow(x, 3) + 6) - Math.Sin(y - a)) / (Math.Log(Math.Pow(x, 4)) - 2 * (Math.Pow(Math.Sin(x), 5)));
 Console.WriteLine($"D={D:F2}");
